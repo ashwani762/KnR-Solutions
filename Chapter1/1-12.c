d11beebd81a6775a9b79aab6b9bfd8f570be53d0
ashwani762 <ashwanikumar.bachchan@gmail.com>
2017-02-24 21:44:51 +0530

    Solving new parts of alphabets

---
+++ Chapter1/1-12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main ()
+
+{
+	int	is_in_word, c;
+
+	is_in_word = 0;
+	while ((c = getchar()) != EOF)
+
+		if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+
+			is_in_word = 1;
+			putchar(c);
+
+		} else {
+
+			if (is_in_word)
+
+				putchar('\n');
+
+			is_in_word = 0;
+
+		}
+}
